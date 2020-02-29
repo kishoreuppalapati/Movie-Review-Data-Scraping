01def3d84442edafba1eb6538752bfd537eb1ceb
MrinmoiHossain <mokit.aust@gmail.com>
2020-03-01 02:51:07 +0600

    server api

--- serve/serve/models.py
+++ serve/serve/models.py
@@ -1,12 +1,8 @@
 from django.db import models
 
-
 # Create your models here.
 class Movie(models.Model):
-    #id = models.IntegerField()
+    id = models.IntegerField(primary_key=True)
     name = models.TextField()
     year = models.IntegerField()
-    awarad = models.CharField(max_length = 25)
-
-    def __str__(self):
-        return self.name
+    awards = models.CharField(max_length = 25)
--- serve/serve/views.py
+++ serve/serve/views.py
@@ -1,10 +1,13 @@
+from django.shortcuts import render
 from django.views.generic import ListView
 
-from . models import Movie
+from .models import Movie
 
 class HomeView(ListView):
     template_name = 'home.html'
-    
+    model = Movie
+
     def get_queryset(self):
-        return Movie.objects.all()
+        query_set = super().get_queryset()
+        return query_set
         
