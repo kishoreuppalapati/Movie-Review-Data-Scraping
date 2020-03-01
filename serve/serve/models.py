f265895b04033bc200b6c8d8702dbd2d827c754b
MrinmoiHossain <mokit.aust@gmail.com>
2020-03-01 11:59:33 +0600

    database problem solved

--- serve/serve/models.py
+++ serve/serve/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Movie(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.TextField()
-    year = models.IntegerField()
-    awards = models.CharField(max_length = 25)
+    Id = models.IntegerField(primary_key=True)
+    Name = models.TextField()
+    YearNo = models.IntegerField()
+    Awards = models.CharField(max_length = 25)
+
+    class Meta:
+        db_table = "Movie"
