e6e9f25002fb21bfa935d332b0adac0fbe605cb2
MrinmoiHossain <mokit.aust@gmail.com>
2020-03-01 23:26:57 +0600

    api setting add

--- serve/MovieList/settings.py
+++ serve/MovieList/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'serve'
+
+    'rest_framework',
+    'serve',
+    'api'
 ]
 
 MIDDLEWARE = [
